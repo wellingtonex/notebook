3dcc0da3d789b7147cc5548e84d8eef753edcf22
wellington.siqueira <wellingtonex@gmail.com>
2017-02-12 12:54:07 -0300

    trabalhando com seeds.rb, responsavel por inserir algumas informações na nossa base de dados

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+puts "Gerando os tipos de contatos(Kinds)..."
+	Kind.create!([{descricption: "Contatos"},{descricption: "Amigos"},{descricption: "Profissional"}])
+puts "Gerando os tipos de contatos(Kinds)... [OK]"
+
+
+puts "Gerando os contatos(Contacts)..."
+	10.times do |i|
+		Contact.create!(
+				nome: Faker::Name.name,
+				email: Faker::Internet.email,
+				kind: Kind.all.sample,
+				rmk: Faker::Lorem.paragraph([1,2,3,4,5].sample)
+			)
+	end
+puts "Gerando os contatos(Contacts)... [OK]"
